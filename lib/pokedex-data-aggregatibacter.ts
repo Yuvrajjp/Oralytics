3c325e9dc93e79a8c816740c30baddbfcbea53be
Yuvrajjp <120201127+Yuvrajjp@users.noreply.github.com>
2025-11-17 23:16:42 -0500

    Create Pokedex entry for Aggregatibacter actinomycetemcomitans

---
+++ lib/pokedex-data-aggregatibacter.ts
@@ -0,0 +1,50 @@
+// lib/pokedex-data-aggregatibacter.ts
+
+/**
+ * Pokedex Entry for Aggregatibacter actinomycetemcomitans
+ * Date: 2025-11-18 04:15:00 UTC
+ */
+
+const aggregatibacterActinomycetemcomitans = {
+    species: "Aggregatibacter actinomycetemcomitans",
+    genes: [
+        "rpmH",
+        "yidD",
+        "era",
+        "grcA",
+        "fbp",
+        "trxA",
+        "miaB",
+        "cdtA",
+        "cdtB",
+        "cdtC",
+        "recA",
+        "rpsG",
+        "rpoH",
+        "exbD",
+        "fur",
+        "oxyR",
+        "sodC",
+        "ruvB",
+        "lpxC"
+    ],
+    alphaFold: {
+        predictions: [
+            { protein: "ProteinName1", pLDT_score: 0.85 },
+            { protein: "ProteinName2", pLDT_score: 0.90 },
+            // More predictions...
+        ]
+    },
+    researchMetrics: {
+        oralMicrobiomeAbundance: "12.3% in diseased sites",
+        biofilmCapability: "78%",
+        virulenceFactors: [
+            "Cdt toxin",
+            "Leukotoxin",
+            "Fimbriae"
+        ],
+        clinicalRelevance: "Aggregatibacter actinomycetemcomitans is associated with periodontal diseases and systemic conditions."
+    }
+};
+
+export default aggregatibacterActinomycetemcomitans;
